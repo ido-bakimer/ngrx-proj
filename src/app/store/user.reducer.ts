179f141aba3e0e1aea3bff156833d4082e2e405d
Ido Bakimer <idobakimer@gmail.com>
2025-10-19 14:40:04 +0300

    2a - reducer and selector updated

--- src/app/store/user.reducer.ts
+++ src/app/store/user.reducer.ts
@@ -1,26 +1,38 @@
 import { createReducer, on } from '@ngrx/store';
+import { createEntityAdapter, EntityState } from '@ngrx/entity';
 import { UserActions } from './user.actions';
 import { User } from '../services/user.service';
 
-export interface UserState {
-  users: User[];
+export const userAdapter = createEntityAdapter<User>({
+  selectId: (user) => user.id, 
+});
+
+export interface UserState extends EntityState<User> {
   loading: boolean;
   error?: string;
 }
 
-export const initialState: UserState = {
-  users: [],
+export const initialState: UserState = userAdapter.getInitialState({
   loading: false,
-};
+  error: undefined,
+});
 
 export const userReducer = createReducer(
   initialState,
-  on(UserActions.loadUsers, (state) => ({ ...state, loading: true })),
-  on(UserActions.loadUsersSuccess, (state, { users }) => ({
+
+  // start loading
+  on(UserActions.loadUsers, (state) => ({
     ...state,
-    users,
-    loading: false,
+    loading: true,
+    error: undefined,
   })),
+
+  // when success - set all users
+  on(UserActions.loadUsersSuccess, (state, { users }) =>
+    userAdapter.setAll(users, { ...state, loading: false })
+  ),
+
+  // failure
   on(UserActions.loadUsersFailure, (state, { error }) => ({
     ...state,
     loading: false,
--- src/app/store/user.selector.ts
+++ src/app/store/user.selector.ts
@@ -1,14 +1,23 @@
-import { createSelector, createFeatureSelector } from '@ngrx/store';
-import { UserState } from './user.reducer';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { userAdapter, UserState } from './user.reducer';
 
 export const selectUserState = createFeatureSelector<UserState>('users');
 
-export const selectUsers = createSelector(
-  selectUserState,
-  (state) => state.users
-);
+// get selectors
+const { selectAll, selectEntities, selectIds, selectTotal } =
+  userAdapter.getSelectors(selectUserState);
 
+export const selectAllUsers = selectAll;
+export const selectUserEntities = selectEntities;
+export const selectUserIds = selectIds;
+export const selectUserTotal = selectTotal;
+
+// if non entity
 export const selectLoading = createSelector(
   selectUserState,
   (state) => state.loading
+);
+export const selectError = createSelector(
+  selectUserState,
+  (state) => state.error
 );
